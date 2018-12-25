51ab560f1c7148bffab27eb7f0ed1d6e362a551e
BONNe <bonne@bonne.id.lv>
2018-12-25 16:13:55 +0200

    Implement SET command for admin. For this command it is necessary to contain <player> and <biome> as arguments in given order. It allows also to specify <updateMode> and <updateRange> but these are not required and if not provided, command will use default ones.

--- src/main/java/world/bentobox/addons/biomes/commands/admin/SetBiomeCommand.java
+++ src/main/java/world/bentobox/addons/biomes/commands/admin/SetBiomeCommand.java
@@ -1,32 +1,38 @@
 package world.bentobox.addons.biomes.commands.admin;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import world.bentobox.addons.biomes.BiomesAddon;
+import world.bentobox.addons.biomes.commands.ExpandedCompositeCommand;
+import world.bentobox.addons.biomes.objects.BiomesObject;
 import world.bentobox.addons.biomes.panel.AdminUserPanel;
+import world.bentobox.addons.biomes.tasks.BiomeUpdateHelper;
+import world.bentobox.addons.biomes.utils.Utils.UpdateMode;
+import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.user.User;
+import world.bentobox.bentobox.util.Util;
 
 
 /**
  * This is admin command that allows to change user biomes.
  */
-public class SetBiomeCommand extends CompositeCommand
+public class SetBiomeCommand extends ExpandedCompositeCommand
 {
-	public SetBiomeCommand(BiomesAddon addon, CompositeCommand parent)
+	public SetBiomeCommand(Addon addon, CompositeCommand parent)
 	{
 		super(addon, parent, "set");
-		this.addon = addon;
 	}
 
 	@Override
 	public void setup()
 	{
 		this.setPermission("admin.biomes");
-		this.setParametersHelp("biomes.admin.parameters");
-		this.setDescription("biomes.admin.description");
+		this.setParametersHelp("biomes.admin.set.parameters");
+		this.setDescription("biomes.admin.set.description");
 	}
 
 
@@ -46,9 +52,37 @@ public class SetBiomeCommand extends CompositeCommand
 		}
 		else
 		{
-			// Process command.
+			User targetUser = this.getPlayer(args, 0, user);
+			BiomesObject biome = this.getBiomeObject(args, 1, user);
+			UpdateMode updateMode = this.getUpdateMode(args, 2, user);
+			int size = this.getUpdateRange(args, 3, user);
 
-			return false;
+			if (targetUser == null || biome == null || updateMode == null || size < 1)
+			{
+				// Show help if something fails.
+				this.showHelp(this, user);
+				return false;
+			}
+			else
+			{
+				// Use BiomeUpdateHelper to change biome for user.
+
+				BiomeUpdateHelper helper = new BiomeUpdateHelper(this.addon,
+					user,
+					targetUser,
+					biome,
+					user.getWorld(),
+					updateMode,
+					size,
+					false);
+
+				if (helper.canChangeBiome())
+				{
+					helper.updateIslandBiome();
+				}
+
+				return true;
+			}
 		}
 	}
 
@@ -56,14 +90,48 @@ public class SetBiomeCommand extends CompositeCommand
 	@Override
 	public Optional<List<String>> tabComplete(User user, String alias, List<String> args)
 	{
-		return Optional.empty();
+		String lastString = args.get(args.size() - 1);
+
+		final List<String> returnList = new ArrayList<>();
+		final int size = args.size();
+
+		switch (size)
+		{
+			case 3:
+				returnList.addAll(Util.tabLimit(new ArrayList<>(Util.getOnlinePlayerList(user)), lastString));
+				break;
+			case 4:
+				List<BiomesObject> biomes = this.addon.getAddonManager().getBiomes();
+
+				// Create suggestions with all biomes that is available for users.
+
+				biomes.forEach(biomesObject -> {
+					returnList.addAll(Util.tabLimit(
+						Collections.singletonList(biomesObject.getBiomeName()), lastString));
+				});
+
+				break;
+			case 5:
+				// Create suggestions with all biomes that is available for users.
+
+				returnList.addAll(Util.tabLimit(Collections.singletonList("ISLAND"), lastString));
+				returnList.addAll(Util.tabLimit(Collections.singletonList("CHUNK"), lastString));
+				returnList.addAll(Util.tabLimit(Collections.singletonList("SQUARE"), lastString));
+
+				break;
+			case 6:
+				if (lastString.isEmpty() || lastString.matches("[0-9]*"))
+				{
+					returnList.addAll(Util.tabLimit(Collections.singletonList("<number>"), lastString));
+				}
+
+				break;
+			default:
+			{
+				break;
+			}
+		}
+
+		return Optional.of(returnList);
 	}
-
-
-// ---------------------------------------------------------------------
-// Section: Variables
-// ---------------------------------------------------------------------
-
-
-	private BiomesAddon addon;
 }
