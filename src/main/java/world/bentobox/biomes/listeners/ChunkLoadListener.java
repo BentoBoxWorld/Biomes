0fdb7656e1507bdfe3a7ae545b098d31f73be189
tastybento <tastybento@wasteofplastic.com>
2020-03-28 11:02:10 -0700

    Does not unload chunks in the event handler
    https://github.com/BentoBoxWorld/Biomes/issues/60

--- src/main/java/world/bentobox/biomes/listeners/ChunkLoadListener.java
+++ src/main/java/world/bentobox/biomes/listeners/ChunkLoadListener.java
@@ -17,76 +17,72 @@ import world.bentobox.biomes.database.objects.BiomeChunkUpdateObject;
  */
 public class ChunkLoadListener implements Listener
 {
-	/**
-	 * Default constructor that inits listener.
-	 * @param addon BiomeAddon object.
-	 */
-	public ChunkLoadListener(BiomesAddon addon)
-	{
-		this.addon = addon;
-	}
+    /**
+     * Default constructor that inits listener.
+     * @param addon BiomeAddon object.
+     */
+    public ChunkLoadListener(BiomesAddon addon)
+    {
+        this.addon = addon;
+    }
 
 
-	/**
-	 * Chunk load event that will force biome change and unload chunk if current chunk is
-	 * loaded by manual process.
-	 * @param event ChunkLoadEvent object.
-	 */
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onChunkLoad(ChunkLoadEvent event)
-	{
-		Chunk chunk = event.getChunk();
+    /**
+     * Chunk load event that will change the biome if required when a chunk is
+     * loaded.
+     * @param event ChunkLoadEvent object.
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onChunkLoad(ChunkLoadEvent event)
+    {
+        Chunk chunk = event.getChunk();
 
-		BiomeChunkUpdateObject updateObject = this.addon.getAddonManager().
-			getPendingChunkUpdateObject(chunk.getWorld(), chunk.getX(), chunk.getZ());
+        BiomeChunkUpdateObject updateObject = this.addon.getAddonManager().
+                getPendingChunkUpdateObject(chunk.getWorld(), chunk.getX(), chunk.getZ());
 
-		if (updateObject != null)
-		{
-			if (updateObject.getMaxX() - updateObject.getMinX() > 15 ||
-				(updateObject.getMinX() >> 4) != chunk.getX() ||
-				(updateObject.getMaxX() >> 4) != chunk.getX())
-			{
-				// Something is wrong with X coordinates. Change to whole chunk.
-				updateObject.setMinX(updateObject.getChunkX() << 4);
-				updateObject.setMaxX(updateObject.getChunkX() << 4 + 15);
-			}
+        if (updateObject != null)
+        {
+            if (updateObject.getMaxX() - updateObject.getMinX() > 15 ||
+                    (updateObject.getMinX() >> 4) != chunk.getX() ||
+                    (updateObject.getMaxX() >> 4) != chunk.getX())
+            {
+                // Something is wrong with X coordinates. Change to whole chunk.
+                updateObject.setMinX(updateObject.getChunkX() << 4);
+                updateObject.setMaxX(updateObject.getChunkX() << 4 + 15);
+            }
 
-			if (updateObject.getMaxZ() - updateObject.getMinZ() > 15 ||
-				(updateObject.getMinZ() >> 4) != chunk.getZ() ||
-				(updateObject.getMaxZ() >> 4) != chunk.getZ())
-			{
-				// Something is wrong with Z coordinates. Change to whole chunk.
-				updateObject.setMinZ(updateObject.getChunkZ() << 4);
-				updateObject.setMaxZ(updateObject.getChunkZ() << 4 + 15);
-			}
+            if (updateObject.getMaxZ() - updateObject.getMinZ() > 15 ||
+                    (updateObject.getMinZ() >> 4) != chunk.getZ() ||
+                    (updateObject.getMaxZ() >> 4) != chunk.getZ())
+            {
+                // Something is wrong with Z coordinates. Change to whole chunk.
+                updateObject.setMinZ(updateObject.getChunkZ() << 4);
+                updateObject.setMaxZ(updateObject.getChunkZ() << 4 + 15);
+            }
 
-			// Update biome from minX till maxX and minZ till maxZ.
-			for (int x = updateObject.getMinX(); x <= updateObject.getMaxX(); x++)
-			{
-				for (int z = updateObject.getMinZ(); z <= updateObject.getMaxZ(); z++)
-				{
-					// Change Biome
-					updateObject.getWorld().setBiome(x, z, updateObject.getBiome());
-				}
-			}
+            // Update biome from minX till maxX and minZ till maxZ.
+            for (int x = updateObject.getMinX(); x <= updateObject.getMaxX(); x++)
+            {
+                for (int z = updateObject.getMinZ(); z <= updateObject.getMaxZ(); z++)
+                {
+                    // Change Biome
+                    updateObject.getWorld().setBiome(x, z, updateObject.getBiome());
+                }
+            }
 
-			// If process is completed, then remove force-load status and unload
-			// chunk.
-			chunk.unload(true);
-
-			// Remove ChunkUpdateObject.
-			this.addon.getAddonManager().removeUpdateObject(updateObject);
-		}
-	}
+            // Remove ChunkUpdateObject.
+            this.addon.getAddonManager().removeUpdateObject(updateObject);
+        }
+    }
 
 
-	// ---------------------------------------------------------------------
-	// Section: Variables
-	// ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // Section: Variables
+    // ---------------------------------------------------------------------
 
 
-	/**
-	 * BiomeAddon variable.
-	 */
-	private BiomesAddon addon;
+    /**
+     * BiomeAddon variable.
+     */
+    private BiomesAddon addon;
 }
