47e02d0ab5dc470de5107a7c7a237e0a398e3701
tastybento <tastybento@wasteofplastic.com>
2020-06-10 21:11:34 -0700

    Update to BentoBox API 1.14
    Short table names for SQL DB's and async DB saving.

--- src/main/java/world/bentobox/biomes/database/objects/BiomeChunkUpdateObject.java
+++ src/main/java/world/bentobox/biomes/database/objects/BiomeChunkUpdateObject.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Biome;
 import com.google.gson.annotations.Expose;
 
 import world.bentobox.bentobox.database.objects.DataObject;
+import world.bentobox.bentobox.database.objects.Table;
 
 
 /**
@@ -16,295 +17,296 @@ import world.bentobox.bentobox.database.objects.DataObject;
  * @author BONNe
  * Created on 2019-07-03
  */
+@Table(name = "BiomeUpdateChunks")
 public class BiomeChunkUpdateObject implements DataObject
 {
-	/**
-	 * Constructor ChunkUpdateObject creates a new ChunkUpdateObject instance.
-	 */
-	public BiomeChunkUpdateObject()
-	{
-	}
+    /**
+     * Constructor ChunkUpdateObject creates a new ChunkUpdateObject instance.
+     */
+    public BiomeChunkUpdateObject()
+    {
+    }
 
 
-	// ---------------------------------------------------------------------
-	// Section: Getters and setters
-	// ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // Section: Getters and setters
+    // ---------------------------------------------------------------------
 
 
-	/**
-	 * This method returns the uniqueId value.
-	 * @return the value of uniqueId.
-	 */
-	@Override
-	public String getUniqueId()
-	{
-		return this.uniqueId;
-	}
+    /**
+     * This method returns the uniqueId value.
+     * @return the value of uniqueId.
+     */
+    @Override
+    public String getUniqueId()
+    {
+        return this.uniqueId;
+    }
 
 
-	/**
-	 * This method sets the uniqueId value.
-	 * @param uniqueId the uniqueId new value.
-	 *
-	 */
-	@Override
-	public void setUniqueId(String uniqueId)
-	{
-		this.uniqueId = uniqueId;
-	}
+    /**
+     * This method sets the uniqueId value.
+     * @param uniqueId the uniqueId new value.
+     *
+     */
+    @Override
+    public void setUniqueId(String uniqueId)
+    {
+        this.uniqueId = uniqueId;
+    }
 
 
-	/**
-	 * This method returns the minX value.
-	 * @return the value of minX.
-	 */
-	public int getMinX()
-	{
-		return minX;
-	}
+    /**
+     * This method returns the minX value.
+     * @return the value of minX.
+     */
+    public int getMinX()
+    {
+        return minX;
+    }
 
 
-	/**
-	 * This method sets the minX value.
-	 * @param minX the minX new value.
-	 *
-	 */
-	public void setMinX(int minX)
-	{
-		this.minX = minX;
-	}
+    /**
+     * This method sets the minX value.
+     * @param minX the minX new value.
+     *
+     */
+    public void setMinX(int minX)
+    {
+        this.minX = minX;
+    }
 
 
-	/**
-	 * This method returns the maxX value.
-	 * @return the value of maxX.
-	 */
-	public int getMaxX()
-	{
-		return maxX;
-	}
+    /**
+     * This method returns the maxX value.
+     * @return the value of maxX.
+     */
+    public int getMaxX()
+    {
+        return maxX;
+    }
 
 
-	/**
-	 * This method sets the maxX value.
-	 * @param maxX the maxX new value.
-	 *
-	 */
-	public void setMaxX(int maxX)
-	{
-		this.maxX = maxX;
-	}
+    /**
+     * This method sets the maxX value.
+     * @param maxX the maxX new value.
+     *
+     */
+    public void setMaxX(int maxX)
+    {
+        this.maxX = maxX;
+    }
 
 
-	/**
-	 * This method returns the minZ value.
-	 * @return the value of minZ.
-	 */
-	public int getMinZ()
-	{
-		return minZ;
-	}
+    /**
+     * This method returns the minZ value.
+     * @return the value of minZ.
+     */
+    public int getMinZ()
+    {
+        return minZ;
+    }
 
 
-	/**
-	 * This method sets the minZ value.
-	 * @param minZ the minZ new value.
-	 *
-	 */
-	public void setMinZ(int minZ)
-	{
-		this.minZ = minZ;
-	}
+    /**
+     * This method sets the minZ value.
+     * @param minZ the minZ new value.
+     *
+     */
+    public void setMinZ(int minZ)
+    {
+        this.minZ = minZ;
+    }
 
 
-	/**
-	 * This method returns the maxZ value.
-	 * @return the value of maxZ.
-	 */
-	public int getMaxZ()
-	{
-		return maxZ;
-	}
+    /**
+     * This method returns the maxZ value.
+     * @return the value of maxZ.
+     */
+    public int getMaxZ()
+    {
+        return maxZ;
+    }
 
 
-	/**
-	 * This method sets the maxZ value.
-	 * @param maxZ the maxZ new value.
-	 *
-	 */
-	public void setMaxZ(int maxZ)
-	{
-		this.maxZ = maxZ;
-	}
+    /**
+     * This method sets the maxZ value.
+     * @param maxZ the maxZ new value.
+     *
+     */
+    public void setMaxZ(int maxZ)
+    {
+        this.maxZ = maxZ;
+    }
 
 
-	/**
-	 * This method returns the chunkX value.
-	 * @return the value of chunkX.
-	 */
-	public int getChunkX()
-	{
-		return chunkX;
-	}
+    /**
+     * This method returns the chunkX value.
+     * @return the value of chunkX.
+     */
+    public int getChunkX()
+    {
+        return chunkX;
+    }
 
 
-	/**
-	 * This method sets the chunkX value.
-	 * @param chunkX the chunkX new value.
-	 *
-	 */
-	public void setChunkX(int chunkX)
-	{
-		this.chunkX = chunkX;
-	}
+    /**
+     * This method sets the chunkX value.
+     * @param chunkX the chunkX new value.
+     *
+     */
+    public void setChunkX(int chunkX)
+    {
+        this.chunkX = chunkX;
+    }
 
 
-	/**
-	 * This method returns the chunkZ value.
-	 * @return the value of chunkZ.
-	 */
-	public int getChunkZ()
-	{
-		return chunkZ;
-	}
+    /**
+     * This method returns the chunkZ value.
+     * @return the value of chunkZ.
+     */
+    public int getChunkZ()
+    {
+        return chunkZ;
+    }
 
 
-	/**
-	 * This method sets the chunkZ value.
-	 * @param chunkZ the chunkZ new value.
-	 *
-	 */
-	public void setChunkZ(int chunkZ)
-	{
-		this.chunkZ = chunkZ;
-	}
+    /**
+     * This method sets the chunkZ value.
+     * @param chunkZ the chunkZ new value.
+     *
+     */
+    public void setChunkZ(int chunkZ)
+    {
+        this.chunkZ = chunkZ;
+    }
 
 
-	/**
-	 * This method returns the biome value.
-	 * @return the value of biome.
-	 */
-	public Biome getBiome()
-	{
-		return biome;
-	}
+    /**
+     * This method returns the biome value.
+     * @return the value of biome.
+     */
+    public Biome getBiome()
+    {
+        return biome;
+    }
 
 
-	/**
-	 * This method sets the biome value.
-	 * @param biome the biome new value.
-	 *
-	 */
-	public void setBiome(Biome biome)
-	{
-		this.biome = biome;
-	}
+    /**
+     * This method sets the biome value.
+     * @param biome the biome new value.
+     *
+     */
+    public void setBiome(Biome biome)
+    {
+        this.biome = biome;
+    }
 
 
-	/**
-	 * This method returns the world value.
-	 * @return the value of world.
-	 */
-	public World getWorld()
-	{
-		return world;
-	}
+    /**
+     * This method returns the world value.
+     * @return the value of world.
+     */
+    public World getWorld()
+    {
+        return world;
+    }
 
 
-	/**
-	 * This method sets the world value.
-	 * @param world the world new value.
-	 *
-	 */
-	public void setWorld(World world)
-	{
-		this.world = world;
-	}
+    /**
+     * This method sets the world value.
+     * @param world the world new value.
+     *
+     */
+    public void setWorld(World world)
+    {
+        this.world = world;
+    }
 
 
-	/**
-	 * This method returns the forceLoaded value.
-	 * @return the value of forceLoaded.
-	 */
-	public boolean isForceLoaded()
-	{
-		return forceLoaded;
-	}
+    /**
+     * This method returns the forceLoaded value.
+     * @return the value of forceLoaded.
+     */
+    public boolean isForceLoaded()
+    {
+        return forceLoaded;
+    }
 
 
-	/**
-	 * This method sets the forceLoaded value.
-	 * @param forceLoaded the forceLoaded new value.
-	 *
-	 */
-	public void setForceLoaded(boolean forceLoaded)
-	{
-		this.forceLoaded = forceLoaded;
-	}
+    /**
+     * This method sets the forceLoaded value.
+     * @param forceLoaded the forceLoaded new value.
+     *
+     */
+    public void setForceLoaded(boolean forceLoaded)
+    {
+        this.forceLoaded = forceLoaded;
+    }
 
 
-	// ---------------------------------------------------------------------
-	// Section: Variables
-	// ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // Section: Variables
+    // ---------------------------------------------------------------------
 
 
-	/**
-	 * Unique ID of current object.
-	 */
-	@Expose
-	private String uniqueId;
+    /**
+     * Unique ID of current object.
+     */
+    @Expose
+    private String uniqueId;
 
-	/**
-	 * Minimal X coordinate of biome update.
-	 */
-	@Expose
-	private int minX;
+    /**
+     * Minimal X coordinate of biome update.
+     */
+    @Expose
+    private int minX;
 
-	/**
-	 * Maximal X coordinate of biome update.
-	 */
-	@Expose
-	private int maxX;
+    /**
+     * Maximal X coordinate of biome update.
+     */
+    @Expose
+    private int maxX;
 
-	/**
-	 * Minimal Z coordinate of biome update.
-	 */
-	@Expose
-	private int minZ;
+    /**
+     * Minimal Z coordinate of biome update.
+     */
+    @Expose
+    private int minZ;
 
-	/**
-	 * Maximal Z coordinate of biome update.
-	 */
-	@Expose
-	private int maxZ;
+    /**
+     * Maximal Z coordinate of biome update.
+     */
+    @Expose
+    private int maxZ;
 
-	/**
-	 * Chunk X coordinate.
-	 */
-	@Expose
-	private int chunkX;
+    /**
+     * Chunk X coordinate.
+     */
+    @Expose
+    private int chunkX;
 
-	/**
-	 * Chunk Z coordinate.
-	 */
-	@Expose
-	private int chunkZ;
+    /**
+     * Chunk Z coordinate.
+     */
+    @Expose
+    private int chunkZ;
 
-	/**
-	 * Biome that will update in current update process.
-	 */
-	@Expose
-	private Biome biome;
+    /**
+     * Biome that will update in current update process.
+     */
+    @Expose
+    private Biome biome;
 
-	/**
-	 * World where current object should work.
-	 */
-	@Expose
-	private World world;
+    /**
+     * World where current object should work.
+     */
+    @Expose
+    private World world;
 
-	/**
-	 * Boolean that indicate that chunk is already force-loaded.
-	 */
-	@Expose
-	private boolean forceLoaded;
+    /**
+     * Boolean that indicate that chunk is already force-loaded.
+     */
+    @Expose
+    private boolean forceLoaded;
 }
