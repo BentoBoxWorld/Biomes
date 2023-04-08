4494cdc09912bb2262f527f79d4452ef0c9a23b6
BONNe <bonne@bonne.id.lv>
2023-04-08 18:53:11 +0300

    PluginAnnotations does not allow setting version dynamically from Maven. Return to a file format. (#117)
    * Create plugin.yml
    
    * Update pom.xml
    
    * Update BiomesPladdon.java

--- src/main/java/world/bentobox/biomes/BiomesPladdon.java
+++ src/main/java/world/bentobox/biomes/BiomesPladdon.java
@@ -7,18 +7,10 @@
 package world.bentobox.biomes;
 
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 
-/**
- * @author tastybento
- */
-@Plugin(name = "Biomes", version = "1.0")
-@ApiVersion(ApiVersion.Target.v1_19)
 public class BiomesPladdon extends Pladdon
 {
     @Override
